8a3e8319c5cf35fa660d6f9dff0a7352b56de751
Jane Costa <yberdysh@gmail.com>
2018-05-11 12:25:02 -0400

    fixing positions

---
+++ server/static/js/spawn-facing.component.js
@@ -0,0 +1,45 @@
+AFRAME.registerComponent('spawn-facing', {
+    schema: {
+        radius: { type: 'number', default: 1 }
+    },
+
+    init: function () {
+        var el = this.el;
+        // var center = el.getAttribute('position');
+
+        // var angleRad = this.getRandomAngleInRadians();
+        // var circlePoint = this.randomPointOnCircle(this.data.radius, angleRad);
+        // var worldPoint = { x: circlePoint.x + center.x, y: center.y, z: circlePoint.y + center.z };
+        // el.setAttribute('position', worldPoint);
+
+        let playerNumber = Object.keys(NAF.entities.entities).length;
+        console.log("playerNumber", playerNumber)
+        if(playerNumber==0){
+          el.setAttribute('position','-1 1.6 -1');
+          el.setAttribute('rotation','0 -90 0');
+        }else if(playerNumber==1){
+          el.setAttribute('position','1 1.6 -1');
+          el.setAttribute('rotation','0 90 0');
+        }else{
+          el.setAttribute('position','3 1.6 -1');
+          el.setAttribute('rotation','0 0 0');
+        }
+
+
+
+        // var angleDeg = angleRad * 180 / Math.PI;
+        // var angleToCenter = -1 * angleDeg + 90;
+        // var rotationStr = '0 ' + angleToCenter + ' 0';
+        // el.setAttribute('rotation', rotationStr);
+    },
+
+    getRandomAngleInRadians: function () {
+        return Math.random() * Math.PI * 2;
+    },
+
+    randomPointOnCircle: function (radius, angleRad) {
+        var x = Math.cos(angleRad) * radius;
+        var y = Math.sin(angleRad) * radius;
+        return { x: x, y: y };
+    }
+});
