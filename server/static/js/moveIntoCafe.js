73cbe2429ff8a2b229252b07d8795079dd83d498
AlisaBelyakova <6183112@gmail.com>
2018-05-14 12:20:01 -0400

    added moveToCafe, cursor highlight, user initial position, fixed level of movement circles

---
+++ server/static/js/moveIntoCafe.js
@@ -0,0 +1,16 @@
+AFRAME.registerComponent('move-on-click', {
+
+    init: function () {
+        this.el.addEventListener('click', function () {
+            var goThere = this.getAttribute('position');
+            var newY = Math.floor(goThere.y + 2), newX = Math.floor(goThere.x + 7), newZ = Math.floor(goThere.z + 1)
+            //for house entry, add extra to x and z (depending) so player is able to enter the house directly  
+            var x = newX.toString(), y = newY.toString(), z = newZ.toString();
+            var newPosition = x + ' ' + y + ' ' + z;
+
+            var sceneEl = document.querySelector('a-scene');
+            sceneEl.querySelector('#player').setAttribute('position', newPosition);
+            sceneEl.querySelector('a-camera').setAttribute('position', newPosition);
+        });
+    }
+});
