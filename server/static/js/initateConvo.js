327cb68e2578f7b55d7781b63d287097977e9756
Jane Costa <yberdysh@gmail.com>
2018-05-15 16:02:54 -0400

    cleaned up code

--- server/static/js/initateConvo.js
+++ server/static/js/initateConvo.js
@@ -1,66 +1,66 @@
 var r = document.getElementById('result');
 
 async function speak(text) {
-    var msg = new SpeechSynthesisUtterance();
-    msg.lang = 'es-ES'
-    console.log(msg)
-    msg.text = text;
-    msg.voice = await speechSynthesis.getVoices().find((voice) => voice.name == "Jorge");
-    console.log("msg.voice", msg.voice)
-    window.speechSynthesis.speak(msg);
+  var msg = new SpeechSynthesisUtterance();
+  msg.lang = 'es-ES'
+  console.log(msg)
+  msg.text = text;
+  msg.voice = await speechSynthesis.getVoices().find((voice) => voice.name == "Jorge");
+  console.log("msg.voice", msg.voice)
+  window.speechSynthesis.speak(msg);
 }
 
 function startConverting() {
-    console.log('start converting ----------> ')
-    if (!('webkitSpeechRecognition' in window)) return
+  console.log('start converting ----------> ')
+  if (!('webkitSpeechRecognition' in window)) return
 
-    const sessionId = uuid.v4()
+  const sessionId = uuid.v4()
 
-    var speechRecognizer = new webkitSpeechRecognition();
-    speechRecognizer.continuous = true;
-    speechRecognizer.interimResults = true;
-    speechRecognizer.lang = 'es-ES';
-    speechRecognizer.start();
+  var speechRecognizer = new webkitSpeechRecognition();
+  speechRecognizer.continuous = true;
+  speechRecognizer.interimResults = true;
+  speechRecognizer.lang = 'es-ES';
+  speechRecognizer.start();
 
-    speechRecognizer.onresult = function (event) {
-        console.log('speech result', event)
+  speechRecognizer.onresult = function(event) {
+    console.log('speech result', event)
 
-        var usersays = ''
-        for (let i = event.resultIndex; i < event.results.length; i++) {
-            if (event.results[i].isFinal) {
-                usersays += event.results[i][0].transcript;
-            }
-        }
-        if (!usersays) return
-
-        const body = JSON.stringify({
-            usersays
-        })
-
-        console.log('body:', body)
-        fetch(`/barista/${sessionId}`, {
-            method: 'PUT',
-            body,
-            headers: new Headers({
-                'Content-Type': 'application/json'
-            })
-        })
-            .then(response => response.json())
-            .then(response => {
-                response = response.toString().toLowerCase();
-                if (response.includes('gracias')) {
-                    speak(response);
-                    speechRecognizer.stop();
-                } else speak(response);
-            })
-            .catch(error => console.error('Error:', error))
+    var usersays = ''
+    for (let i = event.resultIndex; i < event.results.length; i++) {
+      if (event.results[i].isFinal) {
+        usersays += event.results[i][0].transcript;
+      }
     }
+    if (!usersays) return
 
-    return speechRecognizer;
+    const body = JSON.stringify({
+      usersays
+    })
+
+    console.log('body:', body)
+    fetch(`/barista/${sessionId}`, {
+        method: 'PUT',
+        body,
+        headers: new Headers({
+          'Content-Type': 'application/json'
+        })
+      })
+      .then(response => response.json())
+      .then(response => {
+        response = response.toString().toLowerCase();
+        if (response.includes('gracias')) {
+          speak(response);
+          speechRecognizer.stop();
+        } else speak(response);
+      })
+      .catch(error => console.error('Error:', error))
+  }
+
+  return speechRecognizer;
 }
 
 document.getElementById('barista').addEventListener('click', () => {
-    startConverting();
-    document.getElementById('barista').setAttribute('material', 'color:green')
-    document.getElementById('initiate-convo-text').setAttribute('value', 'SPEAK')
+  startConverting();
+  document.getElementById('barista').setAttribute('material', 'color:green')
+  document.getElementById('initiate-convo-text').setAttribute('value', 'SPEAK')
 })
